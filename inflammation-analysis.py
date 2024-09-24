6b5772f0fbd28c97e9f342c2f9662cfe164fe75c
NiekBrouwer98 <niek.brouwer@outlook.com>
2024-09-24 11:46:33 +0200

    Code style fixes

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -2,7 +2,6 @@
 """Software for managing and analysing patients' inflammation data in our imaginary hospital."""
 
 import argparse
-
 from inflammation import models, views
 
 
@@ -12,25 +11,26 @@ def main(args):
     The Controller is responsible for:
     - selecting the necessary models and views for the current task
     - passing data between models and views
+    @type args: input files
     """
-    InFiles = args.infiles
-    if not isinstance(InFiles, list):
-        InFiles = [args.infiles]
+    in_files = args.input_files
+    if not isinstance(in_files, list):
+        in_files = [args.input_files]
 
-
-    for filename in InFiles:
+    for filename in in_files:
         inflammation_data = models.load_csv(filename)
-
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
-
+        view_data = {'average': models.daily_mean(inflammation_data),
+                     'max': models.daily_max(inflammation_data),
+                     'min': models.daily_min(inflammation_data)}
         views.visualize(view_data)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='A basic patient inflammation data management system')
 
     parser.add_argument(
-        'infiles',
+'input_files',
         nargs='+',
         help='Input CSV(s) containing inflammation series for each patient')
 
