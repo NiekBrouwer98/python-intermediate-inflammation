7e1748e3256321c986edf353e8a7770fda88bceb
NiekBrouwer98 <niek.brouwer@outlook.com>
2024-10-08 12:09:15 +0200

    resolved merging errors

--- inflammation/models.py
+++ inflammation/models.py
@@ -8,6 +8,7 @@ and each column represents a single day across all patients.
 """
 
 import numpy as np
+import json
 
 
 def load_csv(filename):
@@ -44,23 +45,8 @@ def daily_min(data):
     """
     return np.min(data, axis=0)
 
-  
-  def load_json(filename):
-    """Load a numpy array from a JSON document.
 
-    Expected format:
-    [
-        {
-            observations: [0, 1]
-        },
-        {
-            observations: [0, 2]
-        }
-    ]
-
-    :param filename: Filename of CSV to load
-
-    """
+def load_json(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         data_as_json = json.load(file)
         return [np.array(entry['observations']) for entry in data_as_json]
